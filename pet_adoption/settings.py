05f3d9439546bf5a0fd01bfb70a33e582d49e446
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-08-19 11:11:10 +0600

    add user serializers

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'drf_yasg',
     'django_filters',
     "debug_toolbar",
