f433c8aed50c11a41a2ace35808537d4d5eefb34
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-08-29 11:18:48 +0600

    is-staf added in user serializer

--- pets/admin.py
+++ pets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from pets.models import Pet, Category, Review
+from pets.models import Pet, Category, Review, PetImage
 
 # Register your models here.
 admin.site.register(Pet)
 admin.site.register(Category)
 admin.site.register(Review)
+admin.site.register(PetImage)
